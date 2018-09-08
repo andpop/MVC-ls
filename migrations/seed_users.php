8306944616385d5dc91819df52c8049e4a27028b
Andrey Popov <andpop@mail.ru>
2018-09-08 23:59:10 +0300

    Make admin panel.

--- migrations/seed_users.php
+++ migrations/seed_users.php
@@ -5,18 +5,20 @@ $faker = Faker\Factory::create();
 
 class User extends Illuminate\Database\Eloquent\Model
 {
-    protected $fillable = ['login', 'name', 'password', 'age', 'description'];//разрешено редактировать только это, остальное запрещено
+    //разрешено редактировать только это, остальное запрещено
+    protected $fillable = ['login', 'password', 'name', 'age', 'avatar_path', 'description'];
     protected $table = 'users';
 }
 
-for($i=0;$i<30;$i++)
+for($i=0;$i<10;$i++)
 {
     $user = new User();
     $user->login = $faker->userName;
-    $user->password = $faker->password;
+    $user->password = password_hash('123', PASSWORD_DEFAULT);;
     $user->name = $faker->name;
     $user->age = $faker->numberBetween(1, 120);
     $user->description = $faker->text;
+    $user->avatar_path = 'http://mvc/img/avatars/work-1.png';
     $user->created_at = $faker->dateTime;
     $user->save();
 }
